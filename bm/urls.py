3869c60d164763313f4c8c9e979fa397a4125c0b
MJ <inchingforward@gmail.com>
2012-10-23 21:40:41 -0500

    Added hello world route

--- bm/urls.py
+++ bm/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bm.views.home', name='home'),
     # url(r'^bm/', include('bm.foo.urls')),
+    url(r'^bookmarks/', include('bm.bookmarks.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
