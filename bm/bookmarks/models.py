56215c02af95cf1b892ee76a4d91ccfcc6db72ed
MJ <inchingforward@gmail.com>
2012-11-04 15:31:07 -0600

    Added Bookmark model

--- bm/bookmarks/models.py
+++ bm/bookmarks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=200)
+    notes = models.TextField(null=True, blank=True)
+    private = models.BooleanField()
+    tags = TaggableManager()
+    user = models.ForeignKey(User)
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-create_date']
+    
+    def __unicode__(self):
+        return self.title
+
