547e77e657cbd12c39b6b85577f8430b86e2afa2
MJ <inchingforward@gmail.com>
2012-12-09 10:55:02 -0600

    Initial bookmark saving

--- bm/bookmarks/urls.py
+++ bm/bookmarks/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'bm.bookmarks.views.index', name='bookmarks_index'),
+    url(r'^save/', 'bm.bookmarks.views.save_bookmark', name='save_bookmark'),
 )
--- bm/bookmarks/views.py
+++ bm/bookmarks/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.forms import ModelForm
 
 from bm.bookmarks.models import Bookmark
 
@@ -8,3 +9,21 @@ from bm.bookmarks.models import Bookmark
 def index(request):
     bookmarks = Bookmark.objects.filter(user=request.user).order_by('-create_date')
     return render(request, "bookmarks/bookmark_list.html", {'bookmarks': bookmarks})
+
+@login_required
+def save_bookmark(request):
+    if request.method == 'GET':
+        form = BookmarkForm()
+    else:
+        form = BookmarkForm(request.POST)
+        if form.is_valid():
+            bookmark = form.save(commit=False)
+            bookmark.user = request.user
+            bookmark.save()
+            
+    return render(request, "bookmarks/save_bookmark.html", {'form': form})
+
+class BookmarkForm(ModelForm):
+    class Meta:
+        model = Bookmark
+        exclude = ('user',)
