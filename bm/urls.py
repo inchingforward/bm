b63983a5c8b63d1539c5c7585d11961959d23321
MJ <inchingforward@gmail.com>
2012-12-19 21:40:39 -0600

    Fixed login/logout redirects.

--- bm/urls.py
+++ bm/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^bookmarks/', include('bm.bookmarks.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^accounts/logout/$','django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='logout'),
+    url(r'^accounts/logout/$','django.contrib.auth.views.logout', {'next_page': '/accounts/login/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
     # Default to showing a user's bookmarks
     url(r'^(\w+)/$', UserBookmarkListView.as_view(), name='user_bookmarks'),
