b5085f23926c211c2150b6d4bc0a8f408dae8a39
MJ <inchingforward@gmail.com>
2013-01-05 19:09:16 -0600

    Added news.

--- bm/news/models.py
+++ bm/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=200)
+    details = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User)
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-create_date']
+    
+    def __unicode__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('news_item', kwargs={'pk': self.pk})
--- bm/views.py
+++ bm/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from bm.news.models import NewsItem
 
 
 def index(request):
     if request.user.is_authenticated():
         return redirect('bookmarks_index')
     
-    return render(request, 'index.html')
+    news = NewsItem.objects.all().order_by('-create_date')[0:5]
+    
+    return render(request, 'index.html', {'news': news})
