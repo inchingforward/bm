61fc3c2ac51e493738972941dcb48b261763192c
inchingforward <inchingforward@gmail.com>
2014-02-10 22:22:24 -0600

    More tweaks for notes.

--- bm/bookmarks/urls.py
+++ bm/bookmarks/urls.py
@@ -5,6 +5,6 @@ from bm.bookmarks.views import BookmarkListView, BookmarkCreateView, BookmarkUpd
 urlpatterns = patterns('',
     url(r'^$', BookmarkListView.as_view(), name='bookmarks_index'),
     url(r'^add$', BookmarkCreateView.as_view(), name='bookmark_create'),
-    url(r'^(?P<pk>\d+)/$', BookmarkUpdateView.as_view(), name='bookmark_update'),
+    url(r'^(?P<pk>\d+)/edit/$', BookmarkUpdateView.as_view(), name='bookmark_update'),
     url(r'^fetchtitle$', 'bm.bookmarks.views.fetch_url_title'),
 )
