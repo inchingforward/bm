b42c68797ff43a121f033c2e2d2e8fd101af6ad6
inchingforward <inchingforward@gmail.com>
2014-02-10 21:04:52 -0600

    Added support for notes.

--- bm/bookmarks/models.py
+++ bm/bookmarks/models.py
@@ -4,14 +4,22 @@ from django.core.urlresolvers import reverse
 from taggit.managers import TaggableManager
 
 
+class Category(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    
+    def __unicode__(self):
+        return self.name
+
 class Bookmark(models.Model):
-    url = models.URLField()
+    url = models.URLField(null=True, blank=True)
     title = models.CharField(max_length=200)
     notes = models.TextField(null=True, blank=True)
     private = models.BooleanField()
     tags = TaggableManager(blank=True)
     user = models.ForeignKey(User)
     create_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, null=True)
     
     class Meta:
         ordering = ['-create_date']
